de43e0281075098d9d3e55799de2a7ffd5048a79
Артём <gridin350@gmail.com>
2024-12-11 18:52:57 +0500

    Task7

--- Tyuiu.GridinAE.Sprint6.Task7.V3.Lib/DataService.cs
+++ Tyuiu.GridinAE.Sprint6.Task7.V3.Lib/DataService.cs
@@ -6,7 +6,27 @@ public class DataService : ISprint6Task7V3
 {
     public int[,] GetMatrix(string path)
     {
-        throw new NotImplementedException();
+        string filePath = path;
+        string fileData = File.ReadAllText(filePath);
+
+        fileData = fileData.Replace('\n', '\r');
+        string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int rows = lines.Length;
+        int columns = lines[0].Split(';').Length;
+
+        int[,] arrayValues = new int[rows, columns];
+
+        for (int r = 0; r < rows; r++)
+        {
+            string[] line_r = lines[r].Split(';');
+            for (int c = 0; c < columns; c++)
+            {
+                arrayValues[r, c] = Convert.ToInt32(line_r[c]);
+            }
+        }
+
+        return arrayValues;
     }
 
     public int[,] GetMatrix(int[,] matrix)
